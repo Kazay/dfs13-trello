bd5c525190ef46ee8c085fffc348a4996389a683
Kevin Czaja <czajakevin@gmail.com>
2019-07-11 17:24:30 +0200

    cards view

--- routes/cards.js
+++ routes/cards.js
@@ -2,4 +2,8 @@ var express = require('express');
 var router = express.Router();
 var server = require('../db');
 
+router.get('/:id', (req, res) => {
+	res.render('cards');
+})
+
 module.exports = router;
